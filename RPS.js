8ef124f0fd034b97790147fea3445a5bddb683fa
Kyle Garner <garnerk10@gmail.com>
2022-12-27 15:59:21 -0500

    finished rps ui

--- RPS.js
+++ RPS.js
@@ -22,25 +22,33 @@ const playGame = (playerSelection, computerSelection) => {
     if (yourPick == computerSelection){
         return "It's a draw!"
     } else if (yourPick == "rock" && computerSelection == "scissors"){
+        player_score++;
         return "You win! Rock beats scissors!"
     } else if (yourPick == "rock" && computerSelection == "paper"){
+        computer_score++;
         return "You lose... paper beats rock."
     }
 
     else if (yourPick == "paper" && computerSelection == "rock"){
+        player_score++;
         return "You win! Paper beats rock!"
     } else if (yourPick == "paper" && computerSelection == "scissors"){
+        computer_score++;
         return "You lose... scissors beats paper."
     }
 
     else if (yourPick == "scissors" && computerSelection == "paper"){
+        player_score++;
         return "You win! Scissors beats paper!"
     } else if (yourPick == "scissors" && computerSelection == "rock"){
+        computer_score++;
         return "You lose... rock beats scissors."}
 };
 
-let playerSelection = "rock";
+let playerSelection = ""
 let computerSelection = getComputerChoice();
+let player_score = 0;
+let computer_score = 0;
 
 const playMatch = (numRounds) => {
     let yourScore = 0;
@@ -67,4 +75,60 @@ Computer's score = ${comScore}
     else{console.log(`It's a draw!`)}
 };
 
-playMatch(7);
+const rock_button = document.getElementById('rock');
+const paper_button = document.getElementById('paper');
+const scissors_button = document.getElementById('scissors');
+const comp_img = document.getElementById('comp_img');
+
+const your_score = document.getElementById('your_score');
+const comp_score = document.getElementById('comp_score');
+let outcome = document.getElementById('outcome');
+
+const getImg = (getPick) => {
+    if (getPick === "rock"){
+        comp_img.src = "rock.png"
+    } else if (getPick === "paper"){
+        comp_img.src = "paper.png"
+    } else if (getPick === "scissors"){
+        comp_img.src = "scissors.png"
+    }
+};
+
+//if player selects the rock button
+let pick_rock = () => {
+    let playerSelection = "rock";
+    let computerSelection = getComputerChoice();
+    outcome.innerText = playGame(playerSelection, computerSelection);
+    your_score.innerText = player_score;
+    comp_score.innerText = computer_score;
+    getImg(computerSelection);
+    comp_img.style.border = "1px solid black";
+};
+
+rock_button.addEventListener("click", pick_rock);
+
+//if player selects the paper button
+let pick_paper = () => {
+    let playerSelection = "paper";
+    let computerSelection = getComputerChoice();
+    outcome.innerText = playGame(playerSelection, computerSelection);
+    your_score.innerText = player_score;
+    comp_score.innerText = computer_score;
+    getImg(computerSelection);
+    comp_img.style.border = "1px solid black";
+};
+
+paper_button.addEventListener("click", pick_paper);
+
+//if player selects the scissors button
+let pick_scissors = () => {
+    let playerSelection = "scissors";
+    let computerSelection = getComputerChoice();
+    outcome.innerText = playGame(playerSelection, computerSelection);
+    your_score.innerText = player_score;
+    comp_score.innerText = computer_score;
+    getImg(computerSelection);
+    comp_img.style.border = "1px solid black";
+};
+
+scissors_button.addEventListener("click", pick_scissors);
